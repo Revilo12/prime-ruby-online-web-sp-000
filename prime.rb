e043f68340418398d6d3729361dbae993d43fb17
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-04 16:10:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,8 @@
 # Add  code here!
 def prime?(input)
-  false if input < 1
-  (2..(input - 1)).all? {|number| input % number != 0}
+  if input < 0
+    false 
+  else
+    (2..(input - 1)).all? {|number| input % number != 0}
+  end
 end
