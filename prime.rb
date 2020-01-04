d44948d85597755d66e004653fc81f339d88ae7b
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-04 16:11:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(input)
-  if input < 2
+  if input < 2 #anythings less than two can't be prime
     false 
   else
     (2..(input - 1)).all? {|number| input % number != 0}
